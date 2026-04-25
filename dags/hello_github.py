17d2333c49bffe365df3bc448e319345a0876c9b
RaphCodeC <46251144+RaphCodec@users.noreply.github.com>
2026-04-25 23:24:14 +0000

    chore: update hello github dag metadata

--- dags/hello_github.py
+++ dags/hello_github.py
@@ -6,6 +6,9 @@ from structlog import get_logger
 log = get_logger()
 
 @dag(
+    dag_id='hello_github',
+    dag_display_name='Hello GitHub',
+    description='A simple DAG to say hello to GitHub',
     start_date=datetime(2025, 8, 25),
     schedule='@hourly',
     catchup=False,
